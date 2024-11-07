eacc91eb948d9446d91f60476869b46e966cca6d
kristal <kristal.gim@mail.ru>
2024-11-07 23:52:54 +0300

    DI

---
+++ DI/DI_3/ConsoleApp1/ConsoleApp1/Movie.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConsoleApp1;
+
+internal class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }// ? - даёт возможность содержать null
+    public DateOnly DateRelease { get; set; }
+
+}
---
+++ DI/DI_3/ConsoleApp1/ConsoleApp1/RepositoryORM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ConsoleApp1;
+
+public class RepositoryORM(ApplicationContext applicationContext)
+{
+    public async List<Movie> GetAll()
+    {
+        return await applicationContext.Movies.ToListAsync();
+    }
+}
