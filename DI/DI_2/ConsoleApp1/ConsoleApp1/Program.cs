712ed712edc6c7e040cd38089e137736bc6cc86d
kristal <kristal.gim@mail.ru>
2024-11-08 23:38:59 +0300

    Di

--- DI/DI_2/ConsoleApp1/ConsoleApp1/Program.cs
+++ DI/DI_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,37 @@
-﻿using ConsoleApp1.Messages;
+﻿using ConsoleApp1;
+using ConsoleApp1.Database.Repositories.Interfaces;
+using ConsoleApp1.Messages;
 using ConsoleApp1.Messages.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-
-//var movieWriter=new MovieWriter(new RepositoryMovieAdo(), new RepositoryMovieORM());
-//await movieWriter.ShowAllMovies();
-//await movieWriter.ShowMovieById(3);
-
+/*
+var movieWriter=new MovieWriter(new RepositoryMovieAdo(), new RepositoryMovieORM());
+await movieWriter.ShowAllMovies();
+await movieWriter.ShowMovieById(3);
+*/
+/*
 IServiceCollection services=new ServiceCollection();
-services.AddTransient<IMessageWrite,ConsoleMessageWrite>();
+services.AddScoped<IMessageWrite,ConsoleMessageWrite>();
+using var serviceProvider = services.BuildServiceProvider();
+using (IServiceScope scope = serviceProvider.CreateScope())
+{
+
+    IMessageWrite messageWrite1 = scope.ServiceProvider.GetService<IMessageWrite>();
+    IMessageWrite messageWrite2 = scope.ServiceProvider.GetService<IMessageWrite>();
+    Console.WriteLine(messageWrite1.rand);
+    Console.WriteLine(messageWrite2.rand);
+}
+using (IServiceScope scope = serviceProvider.CreateScope())
+{
+
+    IMessageWrite messageWrite1 = scope.ServiceProvider.GetService<IMessageWrite>();
+    IMessageWrite messageWrite2 = scope.ServiceProvider.GetService<IMessageWrite>();
+    Console.WriteLine(messageWrite1.rand);
+    Console.WriteLine(messageWrite2.rand);
+}*/
+IServiceCollection services = new ServiceCollection();
+services.AddScoped<IMessageWrite, ConsoleMessageWrite>();
+services.AddTransient<IRepositoryMovie, IRepositoryMovie>();
+services.AddTransient<MovieWriter>();
 using var serviceProvider=services.BuildServiceProvider();
-IMessageWrite? messageWrite1 = serviceProvider.GetService<IMessageWrite>();
-IMessageWrite? messageWrite2 = serviceProvider.GetService<IMessageWrite>();
-Console.WriteLine(messageWrite1.rand);
-Console.WriteLine(messageWrite2.rand);
+var movieWriter=serviceProvider.GetRequiredService<MovieWriter>();
+await movieWriter.ShowMovieById(3);
